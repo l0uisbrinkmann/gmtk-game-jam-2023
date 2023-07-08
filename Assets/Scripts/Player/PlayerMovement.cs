3a0a6405eedf16b660c828131cc68cb2c49efd70
Louis Brinkmann <138525272+l0uisbrinkmann@users.noreply.github.com>
2023-07-08 21:51:37 +0200

    Updated PlayerMovement.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,12 +7,20 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     Rigidbody2D rigidbody;
+    [HideInInspector]
     public Vector2 MoveDirection { get; set; }
+    [HideInInspector]
+    public Vector2 LastMoveDirection { get; set; }
+    [HideInInspector]
+    public float LastHorizontalVector { get; set; }
+    [HideInInspector]
+    public float LastVerticalVector { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        LastMoveDirection = new Vector2(1f, 0f);
     }
 
     // Update is called once per frame
@@ -32,6 +40,21 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         MoveDirection = new Vector2(moveX, moveY).normalized;
+
+        if(MoveDirection.x != 0)
+        {
+            LastHorizontalVector = MoveDirection.x;
+            LastMoveDirection = new Vector2(LastHorizontalVector, 0f);
+        }
+        if(MoveDirection.y != 0)
+        {
+            LastVerticalVector = MoveDirection.y;
+            LastMoveDirection = new Vector2(0f, LastVerticalVector);
+        }
+        if(MoveDirection.x != 0 && MoveDirection.y != 0)
+        {
+            LastMoveDirection = new Vector2(LastHorizontalVector, LastVerticalVector);
+        }
      }
 
     void Move()
